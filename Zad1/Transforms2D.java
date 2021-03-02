01d744fc9a5ac9fff575ada7225dbf0773a91c01
Autarcha <60136139+Autarcha@users.noreply.github.com>
2021-03-02 15:28:21 +0100

    Add files via upload

---
+++ Zad1/Transforms2D.java
@@ -0,0 +1,117 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+
+public class Transforms2D extends JPanel {
+
+    private class Display extends JPanel {
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2 = (Graphics2D)g;
+            g2.translate(300,300);  // Moves (0,0) to the center of the display.
+            int whichTransform = transformSelect.getSelectedIndex();
+            int[] x = new int[17];
+            int[] y = new int[17];
+            for (int i = 1; i < 18; i++)
+            {
+                x[i-1] = (int)(150 * Math.cos((Math.PI/2 + 2*Math.PI*i) /17));
+                y[i-1] = (int)(150 * Math.sin((Math.PI/2 + 2*Math.PI*i) /17));
+            }
+            switch(whichTransform)
+            {
+                case 0:
+                    break;
+                case 1:
+                    g2.scale(0.5, 0.5);
+                    break;
+                case 2:
+                    g2.rotate(Math.toRadians(45));
+                    break;
+                case 3:
+                    g2.scale(0.5,-1);
+                    break;
+                case 4:
+                    g2.shear(0.3,0);
+                    break;
+                case 5:
+                    g2.scale(1,0.5);
+                    g2.translate(0,-453);
+                    break;
+                case 6:
+                    g2.rotate(Math.toRadians(90));
+                    g2.shear(0.3,0);
+                    g2.scale(1.5, 1);
+                    break;
+                case 7:
+                g2.scale(-0.5,-1);
+                    break;
+                case 8:
+                    g2.rotate(Math.toRadians(45));
+                    g2.scale(1,0.5);
+                    g2.translate(0,200);
+                    break;
+                case 9:
+                    g2.shear(0,0.3);
+                    g2.scale(-1,-1);
+                    g2.translate(-150,0);
+
+                    break;
+            }
+            Polygon p = new Polygon(x,y,17);
+            //g2.drawRect(-150, -150, 200,200);
+            //g2.fillRect(-150, -150, 200,200);
+            g2.drawPolygon(p);
+            g2.fillPolygon(p);
+
+            // TODO Apply transforms here, depending on the value of whichTransform!
+
+
+        }
+    }
+
+    private Display display;
+    private BufferedImage pic;
+    private JComboBox<String> transformSelect;
+
+    public Transforms2D() throws IOException {
+        display = new Display();
+        display.setBackground(Color.YELLOW);
+        display.setPreferredSize(new Dimension(600,600));
+        transformSelect = new JComboBox<String>();
+        transformSelect.addItem("None");
+        for (int i = 1; i < 10; i++) {
+            transformSelect.addItem("No. " + i);
+        }
+        transformSelect.addActionListener( new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                display.repaint();
+            }
+        });
+        setLayout(new BorderLayout(3,3));
+        setBackground(Color.GRAY);
+        setBorder(BorderFactory.createLineBorder(Color.GRAY,10));
+        JPanel top = new JPanel();
+        top.setLayout(new FlowLayout(FlowLayout.CENTER));
+        top.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+        top.add(new JLabel("Transform: "));
+        top.add(transformSelect);
+        add(display,BorderLayout.CENTER);
+        add(top,BorderLayout.NORTH);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        JFrame window = new JFrame("2D Transforms");
+        window.setContentPane(new Transforms2D());
+        window.pack();
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        window.setLocation( (screen.width - window.getWidth())/2, (screen.height - window.getHeight())/2 );
+        window.setVisible(true);
+    }
+
+}
